05384bad61da9f931996112aaf865bce0d862d21
alexramz-mxl <alexis.ramirez@laureate.mx>
2025-08-15 19:37:34 -0700

    FirstAssignment

--- LemonRestaurantC2/MenuView.swift
+++ LemonRestaurantC2/MenuView.swift
@@ -18,6 +18,7 @@ struct MenuView: View {
     ]
     
     @State private var showMessage = false
+    @State private var showThankYou = false
     
     var body: some View {
         VStack{
@@ -31,37 +32,60 @@ struct MenuView: View {
             }
             .padding()
             
+            Text("Total items: \(menuItems.count)")
+                .font(.headline)
+                .padding(.horizontal)
+            
             VStack(spacing: 20) {
                 Toggle("Show an special text", isOn: $showMessage)
                     .padding()
-            }
-            
-            if  showMessage {
-                Text("You unlocked a surprise!")
-                    .font(.title3)
-                    .foregroundColor(.green)
-            }
-            
-            // VStack
-            
-            List {
-                ForEach(menuItems.sorted(by: {$0.key > $1.key}), id: \.key){(name, price) in
-                    HStack {
-                        VStack(alignment: .leading){
-                            Text(name)
-                                .font(.headline)
+                Toggle("Show Thank You Message", isOn: $showThankYou)
+                    .padding(.horizontal)
+                
+                
+                if  showMessage {
+                    Text("You unlocked a surprise!")
+                        .font(.title3)
+                        .foregroundColor(.green)
+                }
+                
+                if showThankYou {
+                    Text("Thanks for visiting Little Lemon Restaurant!")
+                    .foregroundColor(.blue)
+                    .italic()
+                    .padding(.horizontal)
+                }
+                // VStack
+                
+                List {
+                    ForEach(menuItems.sorted(by: {$0.key > $1.key}), id: \.key){(name, price) in
+                        HStack {
+                            VStack(alignment: .leading){
+                                Text(name)
+                                    .font(.headline)
+                                
+                                Text("$ \(price, specifier:"%.2f")")
+                                    .foregroundColor(.secondary)
+                            }
                             
-                            Text("$ \(price, specifier:"%.2f")")
-                                .foregroundColor(.secondary)
+                            Spacer()
+                            
+                            if price < 7 {
+                                Text("Value")
+                                    .font(.caption)
+                                    .padding(6)
+                                    .background(Color.green)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(5)
+                            }
                         }
+                        
                     }
-                    
                 }
             }
         }
     }
 }
-
 #Preview {
     MenuView()
 }
